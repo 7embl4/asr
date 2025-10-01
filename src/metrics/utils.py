daa82e3e67f7e72fc0af04d5c1f427beaf267b46
7embl4 <art3mka1408@gmail.com>
2025-10-01 17:08:45 +0300

    Add WER and CER metrics calculation, Update lib requirements

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,13 +1,60 @@
-# Based on seminar materials
+import torch
 
 # Don't forget to support cases when target_text == ''
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    '''
+    Calculates CER metric for a single example of predicted and target texts
+    
+    Params:
+    - pred_text: list of predicted tokens
+    - target_text: list of real tokens
+    Returns:
+    - CER metric (from 0.0 to 1.0)
+    '''
+    N, M = len(target_text), len(predicted_text)
+    cer_mat = torch.zeros(N + 1, M + 1, dtype=torch.int64)
+    cer_mat[0, :] = torch.arange(M + 1).to(dtype=torch.int64)
+    cer_mat[:, 0] = torch.arange(N + 1).to(dtype=torch.int64)
+    
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            cost = int(target_text[i - 1] != predicted_text[j - 1])
+            cer_mat[i, j] = min(
+                cer_mat[i, j - 1] + cost,
+                cer_mat[i - 1, j] + cost,
+                cer_mat[i - 1, j - 1] + cost
+            )
 
+    cer = cer_mat[-1, -1] / N
+    return cer
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    '''
+    Calculates WER metric for a single example of predicted and target texts
+    
+    Params:
+    - target_text: string
+    - predicted_text: string
+    Returns:
+    - WER metric (from 0.0 to 1.0)
+    '''
+    target_tokens = target_text.split()
+    pred_tokens = predicted_text.split()
+    N, M = len(target_tokens), len(pred_tokens)
+    wer_mat = torch.zeros(N + 1, M + 1, dtype=torch.int64)
+    wer_mat[0, :] = torch.arange(M + 1).to(dtype=torch.int64)
+    wer_mat[:, 0] = torch.arange(N + 1).to(dtype=torch.int64)
+
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            cost = int(target_tokens[i - 1] != pred_tokens[j - 1])
+            wer_mat[i, j] = min(
+                wer_mat[i, j - 1] + cost,
+                wer_mat[i - 1, j] + cost,
+                wer_mat[i - 1, j - 1] + cost
+            )
+
+    wer = wer_mat[-1, -1] / N
+    return wer
