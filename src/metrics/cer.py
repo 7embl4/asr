ddfd9273b6197de551c885f928f66f5bdbbd7b0b
7embl4 <art3mka1408@gmail.com>
2025-10-14 22:04:29 +0300

    Add CER and WER beam search metrics

--- src/metrics/cer.py
+++ src/metrics/cer.py
@@ -6,6 +6,9 @@ from torch import Tensor
 from src.metrics.base_metric import BaseMetric
 from src.metrics.utils import calc_cer
 
+from torchaudio.models.decoder._ctc_decoder import ctc_decoder
+
+
 # TODO add beam search/lm versions
 # Note: they can be written in a pretty way
 # Note 2: overall metric design can be significantly improved
@@ -27,3 +30,43 @@ class ArgmaxCERMetric(BaseMetric):
             pred_text = self.text_encoder.ctc_decode(log_prob_vec[:length])
             cers.append(calc_cer(target_text, pred_text))
         return sum(cers) / len(cers)
+
+
+class BeamSearchCERMetric(BaseMetric):
+    def __init__(self, text_encoder, nbest, beam_size, lm=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.decoder = ctc_decoder(
+            lexicon=None,
+            tokens=text_encoder.vocab,
+            lm=lm,
+            nbest=nbest,
+            beam_size=beam_size,
+            blank_token=text_encoder.EMPTY_TOK,
+            sil_token=text_encoder.EMPTY_TOK
+        )
+        self.text_encoder = text_encoder
+
+    def __call__(
+        self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs
+    ):
+        cers = []
+        predictions = log_probs.detach().cpu()
+        lengths = log_probs_length.detach().numpy()
+        for pred, length, target_text in zip(predictions, lengths, text):
+            target_text = self.text_encoder.normalize_text(target_text)
+            pred_text, cer = self._get_best_prediction(pred.unsqueeze(0), target_text)
+            cers.append(cer)
+        return sum(cers) / len(cers)
+    
+    def _get_best_prediction(self, predictions, target_text):
+        hypos = self.decoder(predictions)
+        best_hypo = ""
+        best_cer = 1000
+        for hypo in hypos[0]:
+            hypo_text = self.text_encoder.ctc_decode(hypo.tokens.tolist())
+            hypo_cer = calc_cer(target_text, hypo_text)
+            if hypo_cer <= best_cer:
+                best_hypo = hypo_text
+                best_cer = hypo_cer
+        
+        return best_hypo, best_cer
--- src/metrics/wer.py
+++ src/metrics/wer.py
@@ -6,6 +6,8 @@ from torch import Tensor
 from src.metrics.base_metric import BaseMetric
 from src.metrics.utils import calc_wer
 
+from torchaudio.models.decoder._ctc_decoder import ctc_decoder
+
 # TODO beam search / LM versions
 # Note: they can be written in a pretty way
 # Note 2: overall metric design can be significantly improved
@@ -27,3 +29,43 @@ class ArgmaxWERMetric(BaseMetric):
             pred_text = self.text_encoder.ctc_decode(log_prob_vec[:length])
             wers.append(calc_wer(target_text, pred_text))
         return sum(wers) / len(wers)
+
+
+class BeamSearchWERMetric(BaseMetric):
+    def __init__(self, text_encoder, nbest, beam_size, lm=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.decoder = ctc_decoder(
+            lexicon=None,
+            tokens=text_encoder.vocab,
+            lm=lm,
+            nbest=nbest,
+            beam_size=beam_size,
+            blank_token=text_encoder.EMPTY_TOK,
+            sil_token=text_encoder.EMPTY_TOK
+        )
+        self.text_encoder = text_encoder
+
+    def __call__(
+        self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs
+    ):
+        wers = []
+        predictions = log_probs.detach().cpu()
+        lengths = log_probs_length.detach().numpy()
+        for pred, length, target_text in zip(predictions, lengths, text):
+            target_text = self.text_encoder.normalize_text(target_text)
+            pred_text, wer = self._get_best_prediction(pred.unsqueeze(0), target_text)
+            wers.append(wer)
+        return sum(wers) / len(wers)
+    
+    def _get_best_prediction(self, predictions, target_text):
+        hypos = self.decoder(predictions)
+        best_hypo = ""
+        best_wer = 1000
+        for hypo in hypos[0]:
+            hypo_text = self.text_encoder.ctc_decode(hypo.tokens.tolist())
+            hypo_wer = calc_wer(target_text, hypo_text)
+            if hypo_wer <= best_wer:
+                best_hypo = hypo_text
+                best_wer = hypo_wer
+        
+        return best_hypo, best_wer
